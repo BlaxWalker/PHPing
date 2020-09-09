71b51b3016a5a8cfca21478501eccd725fbdebbe
BlaxWalker <eko.iswinarso@gmail.com>
2020-09-09 15:11:11 +0700

    Update PHPing.bat

--- PHPing.bat
+++ PHPing.bat
@@ -3,5 +3,5 @@ REM Enter the PHP folder first by using 'CD' command to make absulute path insid
 REM Modify the path of PHPing and php folder location if not same with your environment
 
 
-CD "C:\Xampp\htdocs\phping\php\"
+CD /D "C:\Xampp\htdocs\phping\php\"
 "C:\Xampp\php\php.exe" -f PHPing.php
